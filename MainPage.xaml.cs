63509f8b28a4d99224a6e593dea0e633c5817b47
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2024-01-06 01:28:40 +0000

    Added keyboard. Code to get words from GitHub.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,59 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private int points = 0;
+
+        private List<List<string>> userInputLists = new List<List<string>>();
+        private List<List<Frame>> frameLists = new List<List<Frame>>();
+
+        //To get words from GitHub
+        private List<string> words = new List<string>();
+        private HttpClient httpClient = new HttpClient();
+        private Random random = new Random();
+        public string chosenWord { get; set; }
+
 
         public MainPage()
         {
             InitializeComponent();
+            GetWords();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public async void GetWords()
         {
-            count++;
+            var response = await httpClient.GetStringAsync("https://raw.githubusercontent.com/DonH-ITS/jsonfiles/main/words.txt");
+            string[] individualWords = response.Split(new[] { '\n' });
+            words.AddRange(individualWords);
+            chosenWord = PickWord();
+            RandomWordLabel.Text = chosenWord;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+        public string PickWord()
+        {
+            if (words.Count > 0)
+            {
+                int randomNumber = random.Next(0, words.Count);
+                return words[randomNumber];
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
+            {
+                return null;
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnBackButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton(object sender, EventArgs e)
+        {
+
         }
     }
-
 }
